e734bbce88d00be15686939d807fe6737859d186
Denzol255 <den-zoloto@mail.ru>
2020-11-25 19:20:45 +0300

    Create: added desktop and mobile newsletter block

--- js/main.js
+++ js/main.js
@@ -1,16 +1,55 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
-	loop: true,
-	grabCursor: true,
-	autoHeight: true,
+  loop: true,
+  grabCursor: true,
+  // autoHeight: true,
+  // autoHeight слишком сильно сжимает/обрезает одну из картинок
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
   },
 
-	keyboard:{
-		enabled: true,
-		onlyInViewport: true,
-	}
-})
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+});
+
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map(
+      "map",
+      {
+        center: [7.838196, 98.298813],
+        zoom: 17,
+      },
+      {
+        searchControlProvider: "yandex#search",
+      }
+    ),
+    // Создаем геообъект с типом геометрии "Точка".
+    myGeoObject = new ymaps.GeoObject(
+      {
+        // Описание геометрии.
+        geometry: {
+          type: "Point",
+          coordinates: [7.838196, 98.298813],
+        },
+        // Свойства.
+        properties: {
+          // Контент метки.
+          iconContent: "",
+          hintContent: "Hilton",
+        },
+      },
+      {
+        // Опции.
+        // Иконка метки будет растягиваться под размер ее содержимого.
+        preset: 'islands#greenDotIconWithCaption',
+      }
+    );
+
+  myMap.geoObjects
+    .add(myGeoObject);
+}
