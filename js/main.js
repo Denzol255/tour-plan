3124d585f4cd02cc97793d5b9143bed49b6c4e18
Denzol255 <den-zoloto@mail.ru>
2020-12-05 21:49:13 +0300

    Create: added js for modal Fix: make corrections

--- js/main.js
+++ js/main.js
@@ -1,42 +1,43 @@
-var hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
-  grabCursor: true,
-  // autoHeight: true,
-  // autoHeight слишком сильно сжимает/обрезает одну из картинок
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  effect:"coverflow",
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
-});
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+    grabCursor: true,
+    // autoHeight: true,
+    // autoHeight слишком сильно сжимает/обрезает одну из картинок
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    effect: "coverflow",
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+  });
 
-var reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  grabCursor: true,
-  // autoHeight: true,
-  // autoHeight слишком сильно сжимает/обрезает одну из картинок
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    grabCursor: true,
+    // autoHeight: true,
+    // autoHeight слишком сильно сжимает/обрезает одну из картинок
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
-});
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+  });
 
-ymaps.ready(init);
-function init() {
-  var myMap = new ymaps.Map(
+  ymaps.ready(init);
+  function init() {
+    var myMap = new ymaps.Map(
       "map",
       {
         center: [7.838196, 98.298813],
@@ -46,35 +47,59 @@ function init() {
         searchControlProvider: "yandex#search",
       }
     ),
-    // Создаем геообъект с типом геометрии "Точка".
-    myGeoObject = new ymaps.GeoObject(
-      {
-        // Описание геометрии.
-        geometry: {
-          type: "Point",
-          coordinates: [7.838196, 98.298813],
+      // Создаем геообъект с типом геометрии "Точка".
+      myGeoObject = new ymaps.GeoObject(
+        {
+          // Описание геометрии.
+          geometry: {
+            type: "Point",
+            coordinates: [7.838196, 98.298813],
+          },
+          // Свойства.
+          properties: {
+            // Контент метки.
+            iconContent: "",
+            hintContent: "Hilton",
+          },
         },
-        // Свойства.
-        properties: {
-          // Контент метки.
-          iconContent: "",
-          hintContent: "Hilton",
-        },
-      },
-      {
-        // Опции.
-        // Иконка метки будет растягиваться под размер ее содержимого.
-        preset: 'islands#greenDotIconWithCaption',
-      }
-    );
+        {
+          // Опции.
+          // Иконка метки будет растягиваться под размер ее содержимого.
+          preset: "islands#greenDotIconWithCaption",
+        }
+      );
 
-  myMap.geoObjects
-    .add(myGeoObject);
-}
+    myMap.geoObjects.add(myGeoObject);
+  }
 
-var menuButton = document.querySelector(".menu-button")
-menuButton.addEventListener("click", function() {
-  document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--active')
-  document.querySelector(".menu-button").classList.toggle('menu-button--active')
-   document.querySelector("body").classList.toggle('scroll--lock')
-})
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom--active");
+    document
+      .querySelector(".menu-button")
+      .classList.toggle("menu-button--active");
+    document.querySelector("body").classList.toggle("scroll--lock");
+  });
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal)
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass("modal__dialog--visible");
+  }
+});
